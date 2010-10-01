1f8116a2057b3fc713b61854cba15ea45c97acd9
Simon Norberg <simon@dackbrann.net>
2010-10-01 18:25:19 +0200

    now using chroot to only allow access to docroot

--- webserv.c
+++ webserv.c
@@ -4,17 +4,18 @@
 #include <pthread.h>
 #include <limits.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #define BUFF_SIZE 4096
+#define DOCROOT "/var/www"
 
 void * th_response(void *arg)
 {
 	int new_socket = (int)arg;
 	char *message="HTTP/1.0 501 Not Implemented \r\n";
-	char docroot[512]="/var/www/";
 	char *temp;
 	char *uri;
 	char *res;
@@ -27,11 +28,10 @@ void * th_response(void *arg)
 	if (strncmp(buffer, "GET",3) == 0)
 	{
 		temp = buffer; // TODO do we really need this
-		uri = strsep(&temp, " \r\n"); // GET
+		strsep(&temp, " \r\n"); // GET
 		uri = strsep(&temp, " \r\n");
-		strcat(docroot, uri);
-		res = realpath(docroot, NULL);
-		printf("Docroot: %s REALPATH: %s\n",docroot, res);
+		res = realpath(uri, NULL);
+		printf("uri: %s REALPATH: %s\n",uri, res);
 		if(res == NULL)
 		{
 			strcpy(buffer,"HTTP/1.0 404 Not Found \r\n");
@@ -71,31 +71,41 @@ int main()
 {
 	pthread_t ntid;
 	int opt=1;
-	int socket_desc;
+	int master_socket;
 	struct sockaddr_in address;
 	int addrlen;
 	int new_socket;
+	
+	/* tries to chroot the process and then leave privileged mode */
+	chdir(DOCROOT);
+	if (chroot(DOCROOT) == -1)
+	{
+		puts("Server started without chroot permissions\n");
+		puts("exiting\n");
+		return 0;
+	}
+	setuid(1000);
 
-	socket_desc=socket(AF_INET,SOCK_STREAM,0);
+	master_socket=socket(AF_INET,SOCK_STREAM,0);
 
-/* set master socket to allow multiple connections */
-	setsockopt(socket_desc,SOL_SOCKET,SO_REUSEADDR,(char *)&opt,sizeof(opt));
+	/* set master socket to allow multiple connections */
+	setsockopt(master_socket,SOL_SOCKET,SO_REUSEADDR,(char *)&opt,sizeof(opt));
 
-/* type of socket created */
+	/* type of socket created */
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(8080);
-	bind(socket_desc,(struct sockaddr *)&address,sizeof(address));
+	bind(master_socket,(struct sockaddr *)&address,sizeof(address));
 
-/* try to specify maximum of 3 pending connections for the master socket */
-	listen(socket_desc,3);
+	/* maximum pending connections for the master socket */
+	listen(master_socket,5);
 
 	addrlen=sizeof(address);
 	while (1)
 	{
-		new_socket=accept(socket_desc,(struct sockaddr *)&address,&addrlen);
+		new_socket=accept(master_socket,(struct sockaddr *)&address,&addrlen);
 		pthread_create(&ntid, NULL, th_response, (void *) new_socket);
 	}
 	/* shutdown master socket properly */
-	close(socket_desc);
+	close(master_socket);
 }
